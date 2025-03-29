5c568a0d0572a12433bf16324b8475943c369ec6
maharishi17 <maharishi1711@gmail.com>
2025-03-29 17:23:01 +0530

    Fix Netlify MIME type issue

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-
-
 export default defineConfig({
-  
   plugins: [tailwindcss(), react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+  },
 });
